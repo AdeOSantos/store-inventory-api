58ceece26409ba6a9d6753cadc9a550473b9e5a9
Adalberto Santos <adalbertoliveira@gmail.com>
2025-05-23 19:49:24 -0300

    Initial commit.

---
+++ src/main/java/io/github/adeosantos/storeinventoryapi/StoreInventoryApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.adeosantos.storeinventoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreInventoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreInventoryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/adeosantos/storeinventoryapi/StoreInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.adeosantos.storeinventoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreInventoryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
