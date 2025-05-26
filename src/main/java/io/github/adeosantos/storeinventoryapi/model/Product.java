7e09a7022154f8f098cf98b71844919f719b0a42
Adalberto Santos <adalbertoliveira@gmail.com>
2025-05-26 12:19:38 -0300

    Add validations in Product model.

--- src/main/java/io/github/adeosantos/storeinventoryapi/model/Product.java
+++ src/main/java/io/github/adeosantos/storeinventoryapi/model/Product.java
@@ -7,22 +7,30 @@ import lombok.*;
 @Getter
 @Setter
 @Entity
-@Table (name = "products")
+@Table(name = "products")
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    @NotBlank
+    @Column(nullable = false, length = 100)
+    @NotBlank(message = "Product name must not be blank")
+    @Size(max = 100, message = "Product name must be at most 100 characters long")
     private String name;
 
+    @Column(length = 255)
+    @Size(max = 255, message = "Description must be at most 255 characters long")
     private String description;
 
-    @Positive
+    @Column(nullable = false)
+    @Positive(message = "Price must be greater than zero")
     private double price;
 
+    @Column(nullable = false)
+    @Min(value = 0, message = "Quantity cannot be negative")
     private int quantity;
 
+    @Version
+    private Integer version;
 }
